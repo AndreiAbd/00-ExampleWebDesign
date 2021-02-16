72e4dd8af8d88cd9570ecfc9aafbfdf750fecafb
Andrei Abd <usr.g0a1@gmail.com>
2021-02-16 15:33:21 +0200

    adding new lines: css js html

--- js/plugins.js
+++ js/plugins.js
@@ -1,4 +1,19 @@
 /* global $,  jquery, alert */
+// local storage check color item
+let mainColors = localStorage.getItem("color_option");
+if (mainColors !== null){
+	document.documentElement.style.setProperty('--main-color', localStorage.getItem("color_option"));
+};
+//setting box color: switch color setting
+const colorsLi = document.querySelectorAll(".colors-list li");
+colorsLi.forEach(li => {
+	li.addEventListener("click", (e) =>{
+		// set color in Root
+		document.documentElement.style.setProperty('--main-color', e.target.dataset.color);
+		//set color in local storage
+		localStorage.setItem("color_option", e.target.dataset.color);
+	});
+});
 //setting list
 document.querySelector(".fa-cog").onclick = function(){
 	//Fa-Spin Rotation it self
